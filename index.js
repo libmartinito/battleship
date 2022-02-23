8b7cedbadaa10f52b39874d1c4bd979046487df3
lib <lib.martinito@pm.me>
2022-02-23 20:23:58 +0800

    Added the tested Ship, Gameboard, and Player factory functions

---
+++ index.js
@@ -0,0 +1,147 @@
+const Ship = (shipLength, shipName) => {
+  
+  const createHitLocationArr = (length) => {
+    const hitLocationArr = []
+    for(let i = 0; i < length; i += 1) {
+      hitLocationArr.push('not hit')
+    }
+    return hitLocationArr
+  }
+
+  const hitLocationArr = createHitLocationArr(shipLength)
+
+  const hit = (location) => {
+    hitLocationArr[location] = 'hit'
+    return hitLocationArr
+  }
+
+  const isSunk = () => {
+    if(!(hitLocationArr.includes('not hit'))) {
+      return true
+    }
+    return false
+  }
+
+  return{ shipLength, shipName, hit, isSunk }
+}
+
+const Gameboard = () => {
+  const ships = []
+  const shipCoordinates = {}
+  const receivedAttackCoordinates = []
+
+  let shipCounter = 1
+
+  const addToShipCoordinates = (constant, variable, length, orientation, name) => {
+    const constantString = (constant + 9).toString(36)
+
+    shipCoordinates[name] = []
+
+    for(let i = 0; i <= length; i += 1) {
+      const variableString = (variable + 9 + i).toString(36)
+      if(orientation === 'y') {
+        shipCoordinates[name].push(constantString.toUpperCase() + variableString)
+      } else if (orientation === 'x') {
+        shipCoordinates[name].push(variableString.toUpperCase() + constantString)
+      }
+    }
+
+  }
+
+  const shipName = () => {
+    const name = `ship${ shipCounter }`
+    shipCounter += 1
+    return name
+  }
+
+  const placeShip = (start, end) => {
+    const xCoordinateStart = parseInt(start[0], 36) - 9
+    const xCoordinateEnd = parseInt(end[0], 36) - 9
+    const yCoordinateStart = parseInt(start[1], 36) - 9
+    const yCoordinateEnd = parseInt(end[1], 36) - 9
+
+    const xLength = xCoordinateEnd - xCoordinateStart
+    const yLength = yCoordinateEnd - yCoordinateStart
+
+    const shipInstanceName = shipName()
+
+    let shipLength = 0
+    let orientation = ''
+
+    if(xLength === 0) {
+      shipLength = yLength
+      orientation = 'y'
+      addToShipCoordinates(xCoordinateStart, yCoordinateStart, shipLength, orientation, shipInstanceName)
+    } else if (yLength === 0) {
+      shipLength = xLength
+      orientation = 'x'
+      addToShipCoordinates(yCoordinateStart, xCoordinateStart, shipLength, orientation, shipInstanceName)
+    }
+
+    const ship = Ship(shipLength, shipInstanceName)
+    ships.push(ship)
+
+    return ship
+  }
+
+  const getNameFromCoordinate = (obj, coordinate) => Object.keys(obj).find(key => obj[key].includes(coordinate))
+
+  const receiveAttack = (coordinates) => {
+
+    const name = getNameFromCoordinate(shipCoordinates, coordinates)
+    if (name !== undefined) {
+      ships.forEach(ship => {
+        if(ship.shipName === name) {
+          ship.hit()
+        }
+      })
+    } else {
+      receivedAttackCoordinates.push(coordinates)
+    }
+  }
+
+  const areAllShipsSunk = () => {
+    let allShipsSunkBool = true
+
+    ships.forEach(ship => {
+      if(!(ship.isSunk())) {
+        allShipsSunkBool = false
+      }
+    })
+    return allShipsSunkBool
+  }
+
+  return { placeShip, shipCoordinates, receiveAttack, ships, receivedAttackCoordinates, areAllShipsSunk }
+}
+
+const Player = () => {
+  const attackedCoordinates = []
+
+  const attack = (gameboard, coordinates) => {
+    gameboard.receiveAttack(coordinates)
+    attackedCoordinates.push(coordinates)
+  }
+
+  const generateRandomLetter = (max) => String.fromCharCode(Math.random()*max+97)
+  const generateRandomNumber = (min, max) => Math.floor(Math.random()*(max - min + 1) - min)
+
+  const generateCoordinates = () => {
+    const letter = generateRandomLetter(10)
+    const number = generateRandomNumber(1,10)
+    const coordinate = letter.toUpperCase() + number.toString()
+    return coordinate
+  }
+
+  const randomAttack = (gameboard) => {
+    let coordinates = generateCoordinates()
+    while(attackedCoordinates.includes(coordinates)) {
+      coordinates = generateCoordinates()
+    }
+    gameboard.receiveAttack(coordinates)
+    attackedCoordinates.push(coordinates)
+  }
+
+  return { attack, randomAttack }
+}
+
+export { Ship, Gameboard, Player }
---
+++ index.test.js
@@ -0,0 +1,93 @@
+import { Ship, Gameboard, Player } from "./index"
+
+const ship = Ship(1)
+
+describe('hit', () => {
+  const expected = ['not hit', 'hit']
+  it('returns an array with updated hit location', () => {
+    expect(ship.hit(1)).toEqual(expected)
+  })
+})
+
+describe('isSunk', () => {
+  it('returns false when not hit', () => {
+    expect(ship.isSunk()).toBeFalsy();
+  })
+  it('returns true when all locations are hit', () => {
+    ship.hit(0)
+    ship.hit(1)
+    expect(ship.isSunk()).toBeTruthy();
+  })
+})
+
+describe('placeShip', () => {
+  it('returns a ship object given coordinates in vertical orientation', () => {
+    const gameboard = Gameboard()
+    expect(gameboard.placeShip('A1', 'A3').shipLength).toEqual(2)
+  });
+  it('returns a ship object given coordinates in horizontal orientation', () => {
+    const gameboard = Gameboard()
+    expect(gameboard.placeShip('A1', 'C1').shipLength).toEqual(2)
+  });
+  it('updates array of ship coordinates', () => {
+    const gameboard = Gameboard()
+    const expected = {'ship1': ['A1', 'A2', 'A3']}
+    gameboard.placeShip('A1', 'A3')
+    expect(gameboard.shipCoordinates).toEqual(expected)
+  });
+});
+
+describe('receiveAttack', () => {
+  it('sinks a ship when attacks are received for all coordinates of the ship', () => {
+    const gameboard = Gameboard()
+    gameboard.placeShip('A1', 'A3')
+    gameboard.receiveAttack('A1')
+    gameboard.receiveAttack('A2')
+    expect(gameboard.ships[0].isSunk()).toBeTruthy
+  })
+  it('does not sink a ship when attacks are not received for all coordinates of a ship', () => {
+    const gameboard = Gameboard()
+    gameboard.placeShip('A1', 'A3')
+    gameboard.receiveAttack('A1')
+    expect(gameboard.ships[0].isSunk()).toBeFalsy
+  })
+  it('adds the coordinate attacked that did not hit a ship to an array', () => {
+    const gameboard = Gameboard()
+    gameboard.placeShip('A1', 'A3')
+    gameboard.receiveAttack('A4')
+    const expected = ['A4']
+    expect(gameboard.receivedAttackCoordinates).toEqual(expected)
+  })
+})
+
+describe('areAllShipsSunk', () => {
+  it('returns true if all ships are sunk', () => {
+    const gameboard = Gameboard()
+    gameboard.placeShip('A1', 'A2')
+    gameboard.placeShip('B1', 'B2')
+    gameboard.receiveAttack('A1')
+    gameboard.receiveAttack('A2')
+    gameboard.receiveAttack('B1')
+    gameboard.receiveAttack('B2')
+    expect(gameboard.areAllShipsSunk()).toBeTruthy
+  })
+  it('returns false if all ships are not sunk', () => {
+    const gameboard = Gameboard()
+    gameboard.placeShip('A1', 'A2')
+    gameboard.placeShip('B1', 'B2')
+    gameboard.receiveAttack('A1')
+    gameboard.receiveAttack('A2')
+    expect(gameboard.areAllShipsSunk()).toBeFalsy
+  })
+})
+
+describe('attack', () => {
+  it('attacks a coordinate on a gameboard', () => {
+    const gameboard = Gameboard()
+    gameboard.placeShip('A1', 'A2')
+    const player = Player()
+    player.attack(gameboard, 'A1')
+    player.attack(gameboard, 'A2')
+    expect(gameboard.areAllShipsSunk()).toBeTruthy
+  })
+})
