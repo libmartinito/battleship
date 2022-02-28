a82e6f72fe9b3064293dbebd9ada622cef721ea9
lib <lib.martinito@pm.me>
2022-02-28 14:56:36 +0800

    Modified the placeShip function

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 // A factory function for the ship object
 
-const Ship = (shipLength) => {
+const Ship = (shipLength, shipName) => {
   // A helper function that creates an array of hit locations
   const setShipHitArr = (length) => {
     const shipHitArr = []
@@ -27,17 +27,20 @@ const Ship = (shipLength) => {
     return true
   }
 
-  return{ shipLength, hit, isSunk, getShipHitArr }
+  return{ shipLength, shipName, hit, isSunk, getShipHitArr }
 }
 
 // A factory function for the gameboard object
 
 const Gameboard = () => {
   const ships = []
-  const shipCoordinatesArr = []
+  const shipCoordinatesArr = {}
+  const missedAttacksArr = []
+  let shipCounter = 1
 
   const getShipCoordinatesArr = () => shipCoordinatesArr
 
+  // A function that adds ship coordinates to an array and makes a ship object
   const placeShip = (start, end) => {
     const startX = parseInt(start[0], 36) - 9
     const startY = parseInt(start[1], 10)
@@ -47,17 +50,35 @@ const Gameboard = () => {
     const xLength = endX - startX
     const yLength = endY - startY
 
+    const shipName = `Ship${shipCounter}`
+    let shipLength = 0
+    const coordinatesArr = []
+
     if(xLength === 0) {
+      shipLength = yLength
       for(let i = 0; i < yLength + 1; i += 1) {
-        shipCoordinatesArr.push((startX + 9).toString(36).toUpperCase() + (startY + i).toString())
+        coordinatesArr.push((startX + 9).toString(36).toUpperCase() + (startY + i).toString())
       }
+      shipCoordinatesArr[shipName] = coordinatesArr
     } else {
+      shipLength = xLength
       for(let i = 0; i < xLength + 1; i += 1) {
-        shipCoordinatesArr.push((startX + i + 9).toString(36).toUpperCase() + (startY).toString())
+        coordinatesArr.push((startX + i + 9).toString(36).toUpperCase() + (startY).toString())
       }
+      shipCoordinatesArr[shipName] = coordinatesArr
     }
-    const ship = Ship()
+    
+    const ship = Ship(shipLength, shipName)
     ships.push(ship)
+    shipCounter += 1
+  }
+
+  // A function that determines if a ship has been hit, sends the hit function
+  // to said ship and collects missed attacks in an array
+  const receiveAttack = (coordinates) => {
+    if(shipCoordinatesArr.includes(coordinates)) {
+
+    }
   }
 
   return { placeShip, getShipCoordinatesArr }
